83dd7de3ef554c2de97ef22d63fdebd16f455033
fajri <fajriwip@gmail.com>
2022-11-23 17:37:56 +0700

    finished group motion

--- shapes/RectangleGroup.js
+++ shapes/RectangleGroup.js
@@ -1,15 +1,50 @@
 import { two } from '../two';
+import { fullRotation } from '../utils';
 
 export default function RectangleGroup() {
-    const numberOfShape = 12;
+    const numberOfShape = 40;
     const plotRadius = 150;
+    const halfWidth = two.width * 0.5;
+    const halfHeight = two.height * 0.5;
 
-    for (let i = 0; i < numberOfShape; i += i) {
-        const x = i * 30 + 30;
-        const y = two.width * 0.5;
+    const shapes = [];
 
-        const shape = two.makeRectangle(x, y, 20, 20);
+    for (let i = 0; i < numberOfShape; i++) {
+        const angle = fullRotation * i / numberOfShape;
+        const x = plotRadius * Math.cos(angle);
+        const y = plotRadius * Math.sin(angle);
+
+        const shape = two.makeRectangle(x, y, 150, 10);
         shape.fill = '#f9bc31';
         shape.noStroke();
+        shape.rotation = angle;
+
+        shapes.push(shape);
     }
+
+    const group = two.makeGroup(shapes);
+    group.translation.set(halfWidth, halfHeight);
+
+    let scale;
+    scale = 1;
+
+    let isScaled;
+    isScaled = true;
+
+    two.bind('update', () => {
+        group.rotation += 0.00015;
+
+        // grow
+        isScaled && (scale += 0.005);
+        scale >= 2 && (isScaled = false);
+
+        // shrink
+        !isScaled && (scale -= 0.005);
+        scale <= 0.05 && (isScaled = true);
+
+        shapes.forEach((shape) => {
+            shape.rotation += 0.025;
+            shape.scale = scale;
+        });
+    });
 }
